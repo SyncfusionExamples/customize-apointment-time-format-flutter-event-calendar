1506b8c606c9cc3fe540fb829f262f55a669d401
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-10-26 17:55:15 +0530

    FLUT-2629 - Sample added.

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(AppointmentTimeTextFormat());
+
+class AppointmentTimeTextFormat extends StatefulWidget {
+  @override
+  AppointmentTimeTextFormatState createState() => AppointmentTimeTextFormatState();
+}
+
+List<String> views = <String>[
+  'Month',
+  'Schedule'
+];
+
+class AppointmentTimeTextFormatState extends State<AppointmentTimeTextFormat> {
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    _calendarController = CalendarController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              leading: PopupMenuButton<String>(
+                icon: Icon(Icons.calendar_today),
+                itemBuilder: (BuildContext context) =>
+                    views.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList(),
+                onSelected: (String value) {
+                  setState(() {
+                    if (value == 'Month') {
+                      _calendarController.view = CalendarView.month;
+                    } else if(value=="Schedule")
+                      {
+                        _calendarController.view=CalendarView.schedule;
+                      }
+                  });
+                },
+              ),
+            ),
+            body: SafeArea(
+              child: SfCalendar(
+                controller: _calendarController,
+                dataSource: _getDataSource(),
+                monthViewSettings: MonthViewSettings(showAgenda: true),
+                appointmentTimeTextFormat: 'hh:mm:ss a',
+              ),
+            )));
+  }
+
+  _DataSource _getDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 4)),
+      endTime: DateTime.now().add(Duration(hours: 5)),
+      subject: 'Meeting',
+      color: Colors.red,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 5)),
+      subject: 'Development Meeting   New York, U.S.A',
+      color: Color(0xFFf527318),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      subject: 'Project Plan Meeting   Kuala Lumpur, Malaysia',
+      color: Color(0xFFfb21f66),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      subject: 'Support - Web Meeting   Dubai, UAE',
+      color: Color(0xFFf3282b8),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 1)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      subject: 'Project Release Meeting   Istanbul, Turkey',
+      color: Color(0xFFf2a7886),
+    ));
+    appointments.add(Appointment(
+        startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+        endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+        subject: 'Release Meeting',
+        color: Colors.lightBlueAccent,
+        isAllDay: true));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -4)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -4)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 11, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 12, days: -2)),
+      subject: 'Customer Meeting   Tokyo, Japan',
+      color: Color(0xFFffb8d62),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
